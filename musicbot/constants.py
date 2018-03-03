55915f213cab37260490f5e1b80bb05ded89288c
Jimgersnap <35190327+Jimgersnap@users.noreply.github.com>
2018-03-03 04:24:49 -0800

    Using sub-version for DJ-Roomba version info

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,7 +1,7 @@
 import os.path
 
 MAIN_VERSION = '1.9.6_3'
-SUB_VERSION = ''
+SUB_VERSION = '-1.13b'
 VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
