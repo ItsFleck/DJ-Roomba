a264e70ea1b4dd89e6b7693378291568838b694d
ItsFleck <zxirius@live.com>
2018-04-13 12:11:11 -0700

    Updated version info to 1.2.1-beta

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,7 +1,7 @@
 import os.path
 
 MAIN_VERSION = '1.9.7'
-SUB_VERSION = '-1.2-beta'
+SUB_VERSION = '-1.2.1-beta'
 VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
